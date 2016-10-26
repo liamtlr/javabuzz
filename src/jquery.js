e7f1513ff5ae4687cd6f03f67a63e0d3f4141bb9
Liam Taylor <liam.taylor888@googlemail.com>
2016-10-26 12:10:55 +0100

    Added addition linkage between jqery.js and index.html

--- src/jquery.js
+++ src/jquery.js
@@ -1,5 +1,6 @@
 $( document ).ready(function() {
 	thermostat = new Thermostat;
+	$('#temperature_display').text(thermostat.getCurrentTemperature());
 });
 
 $('#reset_button').click(function() {
@@ -8,4 +9,23 @@ $('#reset_button').click(function() {
 
 $('#increase_temperature_button').click(function() {
 	thermostat.increaseTemperature();
-}); 
+	$('#temperature_display').text(thermostat.getCurrentTemperature());
+
+});
+
+$('#decrease_temperature').click(function(){
+	thermostat.decreaseTemperature();
+	$('#temperature_display').text(thermostat.getCurrentTemperature());
+});
+
+$('#turn_on_power_saving').click(function(){
+	thermostat.turnOnPowerSavingMode();
+});
+
+$('#turn_off_power_saving').click(function(){
+	thermostat.turnOffPowerSavingMode();
+});
+
+$('button').click(function(){
+	console.log(this)
+})
