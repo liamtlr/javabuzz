002c809b2522d4ba67b5830c1296f54f7824521e
Tomy8s <tomy8s@hotmail.com>
2016-10-27 15:30:16 +0100

    integrated weaher API into page

---
+++ src/weather.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  var showWeather = function() {
+    if($("#city").val() === ""){
+      $("#show_weather").hide();
+    } else {
+      $("#show_weather").show();
+    }
+  }
+
+  showWeather();
+
+  $("#city").keyup(function (){
+    var city = $("#city").val();
+    $.getJSON('http://api.openweathermap.org/data/2.5/weather?q='+ city +'&units=metric&appid=a3d9eb01d4de82b9b8d0849ef604dbed', function(weather){
+      $("#city_span").text(weather.name + ", " + weather.sys.country);
+      $("#current_weather").text(weather.weather[0].description);
+      $("#temperature_outside").text(Math.round(weather.main.temp));
+    });
+    showWeather();
+  });
+});
